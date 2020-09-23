e2a64c10867fc3b3abdd4a3b3556cf2443940c45
David Stinnett <davidstinnett@icloud.com>
2020-09-23 17:16:52 -0400

    simplified code

--- JS/game.js
+++ JS/game.js
@@ -4,7 +4,6 @@ const question = document.querySelector('#question');
 const choices = Array.from(document.querySelectorAll('.choice-text'));
 const progressText = document.querySelector('#progressText');
 const scoreText = document.querySelector('#score');
-const progressBarFull = document.querySelector('#progressBarFull');
 const timerEl = document.querySelector('.time')
 
 //global variables that are called in later functions//
